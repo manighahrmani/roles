7dd67a7b9560d089d85fe6ad1724131da3018db4
manighahrmani <mashapicasso@gmail.com>
2023-09-08 18:22:06 +0100

    Try to do it without commands

--- bot.js
+++ bot.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
 const csv = require('csv-parser');
-
 const { Client, GatewayIntentBits } = require('discord.js');
 
 const client = new Client({
@@ -11,66 +10,61 @@ const client = new Client({
   ],
 });
 
-
 const TOKEN = 'MTE0OTc0MDU2NDg4ODE1ODM1OQ.G-2QOr.ybjqnFGNO4W9iFt_yqOmsn7dF4rtyVV9NMxtQE';
 
-client.on('ready', () => {
+client.on('ready', async () => {
   console.log(`Logged in as ${client.user.tag}`);
-});
 
-client.on('message', async (message) => {
-  console.log(`Received message: ${message.content}`);
-  if (message.content === '!updateRoles') {
-    const guild = message.guild;
-    const placementStudentRole = guild.roles.cache.find((role) => role.name === 'placement-student');
+  // Your role-updating code here
+  const guild = client.guilds.cache.first(); // Assuming the bot is only in one guild
+  const placementStudentRole = guild.roles.cache.find((role) => role.name === 'placement-student');
 
-    if (!placementStudentRole) {
-      console.error('Error: Role "placement-student" not found.');
-      return;
-    }
+  if (!placementStudentRole) {
+    console.error('Error: Role "placement-student" not found.');
+    return;
+  }
 
-    try {
-      const data = [];
-      fs.createReadStream('data.csv')
-        .pipe(csv())
-        .on('data', (row) => {
-          data.push(row);
-        })
-        .on('end', () => {
-          // Process the data and update roles for placement students
-          const placementStudents = guild.members.cache.filter((member) =>
-            member.roles.cache.has(placementStudentRole.id),
-          );
+  try {
+    const data = [];
+    fs.createReadStream('data.csv')
+      .pipe(csv())
+      .on('data', (row) => {
+        data.push(row);
+      })
+      .on('end', () => {
+        // Process the data and update roles for placement students
+        const placementStudents = guild.members.cache.filter((member) =>
+          member.roles.cache.has(placementStudentRole.id),
+        );
 
-          placementStudents.forEach((student) => {
-            // Extract student ID from nickname
-            const nickname = student.displayName;
-            const idMatch = nickname.match(/UP(\d{5,7})/i);
+        placementStudents.forEach((student) => {
+          // Extract student ID from nickname
+          const nickname = student.displayName;
+          const idMatch = nickname.match(/UP(\d{5,7})/i);
 
-            if (!idMatch) {
-              console.error(`Error: Invalid nickname format for ${nickname}`);
-              return;
-            }
+          if (!idMatch) {
+            console.error(`Error: Invalid nickname format for ${nickname}`);
+            return;
+          }
 
-            const studentId = idMatch[1];
-            const matchingRow = data.find((row) => row['Student No'] === studentId);
+          const studentId = idMatch[1];
+          const matchingRow = data.find((row) => row['Student No'] === studentId);
 
-            if (matchingRow && matchingRow['Block Number'] === '4') {
-              // Remove existing roles
-              student.roles.remove([...student.roles.cache.keys()]);
+          if (matchingRow && matchingRow['Block Number'] === '4') {
+            // Remove existing roles
+            student.roles.remove([...student.roles.cache.keys()]);
 
-              // Add new roles
-              student.roles.add([
-                '1149738387033559140', // test
-                '1149706993813176452', // L6
-                getCourseRole(matchingRow.Course), // Get the course role based on CSV data
-              ]);
-            }
-          });
+            // Add new roles
+            student.roles.add([
+              '1149738387033559140', // test
+              '1149706993813176452', // L6
+              getCourseRole(matchingRow.Course), // Get the course role based on CSV data
+            ]);
+          }
         });
-    } catch (error) {
-      console.error('Error reading CSV file:', error);
-    }
+      });
+  } catch (error) {
+    console.error('Error reading CSV file:', error);
   }
 });
 
