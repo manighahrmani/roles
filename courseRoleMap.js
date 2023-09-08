a22e3cb013f17201a1f14459a9032b014d2cb774
manighahrmani <mashapicasso@gmail.com>
2023-09-08 20:23:39 +0100

    Put role map in separate file

---
+++ courseRoleMap.js
@@ -0,0 +1,9 @@
+export const courseRoleMap = {
+  'BSC (HONS) COMPUTER SCIENCE': 'compsi',
+  'BSC (HONS) BUSINESS INFORMATION SYSTEMS': 'bis',
+  'BSC (HONS) COMPUTING': 'computing',
+  'BSC (HONS) SOFTWARE ENGINEERING': 'softeng',
+  'BSC (HONS) COMPUTER NETWORKS': 'networks-bsc',
+  'BSC (HONS) DATA SCIENCE AND ANALYTICS': 'datascience-bsc',
+  'BSC (HONS) CYBER SECURITY AND FORENSIC COMPUTING': 'cyfor',
+};
