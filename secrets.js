ea7969d505d2ee14086d7e6dee074976c47f2101
manighahrmani <mashapicasso@gmail.com>
2023-09-10 20:30:25 +0100

    Delete secrets.js

--- secrets.js
+++
@@ -1,4 +0,0 @@
-export const TOKEN = "MTE0OTc0MDU2NDg4ODE1ODM1OQ.GJlRCw.w4vMsM8uwSG7DHcsCTxzfHjCMLW_9QZwacpxtU";
-export const SERVER_ID = "760155974467059762";
-export const CLIENT_ID = "1149740564888158359";
-export const PATH_TO_CSV = "./data.csv";
