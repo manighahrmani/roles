baa91d9133d31752fd3bf4536c2d048e6db64c49
manighahrmani <mashapicasso@gmail.com>
2023-09-10 21:04:30 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
-// Select members with this role:
-export const SELECT_ROLE = 'placement-student';
+// Select members with this role (ID):
+export const SELECT_ROLE = '761289612068388874';
+export const SELECT_ROLE_NICKNAME = 'placement-student';
 
 // Who have this block number in the CSV file:
 export const BLOCK = '4';
@@ -9,13 +10,13 @@ export const BLOCK = '4';
 // values are the role names from Discord
 export function getCourseRole(courseName) {
   const courseRoleMap = {
-    'BSC (HONS) COMPUTER SCIENCE': 'compsi',
-    'BSC (HONS) BUSINESS INFORMATION SYSTEMS': 'bis',
-    'BSC (HONS) COMPUTING': 'computing',
-    'BSC (HONS) SOFTWARE ENGINEERING': 'softeng',
-    'BSC (HONS) COMPUTER NETWORKS': 'networks-bsc',
-    'BSC (HONS) DATA SCIENCE AND ANALYTICS': 'datascience-bsc',
-    'BSC (HONS) CYBER SECURITY AND FORENSIC COMPUTING': 'cyfor',
+    'BSC (HONS) COMPUTER SCIENCE': '1149706797553303655', // comsci
+    'BSC (HONS) BUSINESS INFORMATION SYSTEMS': '1149707295656267789', // bis
+    'BSC (HONS) COMPUTING': '1149707654906789889', // computing
+    'BSC (HONS) SOFTWARE ENGINEERING': '1149707019692036156', // softeng
+    'BSC (HONS) COMPUTER NETWORKS': '1149707187401269309', // networks-bsc
+    'BSC (HONS) DATA SCIENCE AND ANALYTICS': '1149707119784894657', // datascience-bsc
+    'BSC (HONS) CYBER SECURITY AND FORENSIC COMPUTING': '1149707254514335875', // cyfor
   };
 
   return courseRoleMap[courseName] || null;
@@ -23,7 +24,7 @@ export function getCourseRole(courseName) {
 
 // Roles (ID) to add to selected members
 export const ADDED_ROLES = [
-  '1149738387033559140', // test
+  '1150521303745441996', // test1
   '1149706993813176452', // L6
 ];
 
